859ec2143dc1e21983b663aa6a98ad156972c59b
Justin Morris <justin@pixelbloom.com>
2013-04-05 12:19:47 +1100

    Cleaned up path
    Conflicts:
    	system/path.zsh

--- system/path.zsh
+++ system/path.zsh
@@ -1,2 +1,6 @@
+<<<<<<< HEAD
 export PATH="./bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
+=======
+export PATH="$HOME/bin:./bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
+>>>>>>> b282701... Cleaned up path
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
