1edd263caf4aed7cb37265d37ffea4e6762e7760
Justin Morris <justin@pixelbloom.com>
2013-06-06 10:55:57 +1000

    Use boxen zsh-syntax-highlighting

---
+++ zsh/highlighting.zsh
@@ -0,0 +1,2 @@
+# Assumes that zsh-syntax-highlighting is being provided via homebrew/boxen
+[ -f /opt/boxen/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ] && source /opt/boxen/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
--- zsh/zsh-syntax-highlighting.zsh
+++
@@ -1,1 +0,0 @@
-[ -f /usr/local/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ] && source /usr/local/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
