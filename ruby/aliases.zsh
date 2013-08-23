eff4c92f9720db3ef544370c628284e479f729c4
plasticine <justin@pixelbloom.com>
2013-08-23 11:08:22 +1000

    Update a few things

---
+++ ruby/aliases.zsh
@@ -0,0 +1,3 @@
+alias fs='foreman start'
+alias rs='rails server'
+alias rc='rails console'
---
+++ zsh/completions.zsh
@@ -0,0 +1,2 @@
+# Assumes that zsh-completions is being provided via homebrew/boxen
+fpath=(/opt/boxen/homebrew/share/zsh-completions/ $fpath)
