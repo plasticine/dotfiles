12b16ad061fd6cf48435cdc13a365538be09cf74
Justin Morris <desk@pixelbloom.com>
2016-06-24 09:53:21 +1000

    WIP

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -3,3 +3,15 @@ alias dcu='dc up'
 alias dcb='dc build'
 alias dcrm='dc rm'
 alias dck='dc kill'
+
+# Stop all containers
+dstop() { docker stop $(docker ps -a -q); }
+
+# Remove all containers
+drm() { docker rm $(docker ps -a -q); }
+
+# Stop and Remove all containers
+alias drmf='docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)'
+
+# Remove all images
+dri() { docker rmi $(docker images -q); }
--- system/boxen.zsh
+++ system/boxen.zsh
@@ -1,1 +1,2 @@
+
 [ -f /opt/boxen/env.sh ] && source /opt/boxen/env.sh
