980424294ca5eb444ceb648a1d23ac8e27a4bad3
Justin Morris <desk@pixelbloom.com>
2016-10-02 09:26:44 +1100

    Cleaned up git prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,3 +1,5 @@
+setopt prompt_subst
+
 function welcome() {
   echo "
 $(tput setaf 2)`date +"%A, %e %B %Y, %r"`
@@ -18,69 +20,128 @@ else
   git="/usr/bin/git"
 fi
 
-get_hostname() {
-  hostname=$(hostname -f)
-  if [[ "$SSH_CONNECTION" != '' ]]
-  then
-    echo "%{$fg_bold[magenta]%}[$hostname] %{$reset_color%}"
-  else
-    echo "@$hostname"
+GIT_PROMPT_AHEAD="%{$fg[green]%}A:NUM%{$reset_color%}"
+GIT_PROMPT_BEHIND="%{$fg[red]%}B:NUM%{$reset_color%}"
+GIT_PROMPT_MERGING="%{$fg_bold[magenta]%}⚡︎%{$reset_color%}"
+GIT_PROMPT_UNTRACKED="%{$fg_bold[red]%}●%{$reset_color%}"
+GIT_PROMPT_MODIFIED="%{$fg_bold[yellow]%}●%{$reset_color%}"
+GIT_PROMPT_STAGED="%{$fg_bold[green]%}●%{$reset_color%}"
+GIT_PROMPT_NULL_STATE="%{$fg[white]%}●%{$reset_color%}"
+
+function parse_git_branch() {
+  ($git symbolic-ref -q HEAD || $git name-rev --name-only --no-undefined --always HEAD) 2> /dev/null
+}
+
+function git_has_tracking_branch() {
+  echo "!"
+}
+
+function git_num_commits_ahead() {
+  local commits_ahead="$($git log --oneline @{u}.. 2> /dev/null | wc -l | tr -d ' ')"
+  if [ "$commits_ahead" -gt 0 ]; then
+    echo ${GIT_PROMPT_AHEAD//NUM/$commits_ahead}
   fi
 }
 
-git_head_id () {
-  echo "$($git rev-parse --short HEAD 2>/dev/null)"
+function git_num_commits_behind() {
+  local commits_behind="$(git log --oneline ..@{u} 2> /dev/null | wc -l | tr -d ' ')"
+  if [ "$commits_behind" -gt 0 ]; then
+    echo ${GIT_PROMPT_BEHIND//NUM/$commits_behind}
+  fi
 }
 
-git_branch () {
-  echo $($git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})
+function git_requires_merge() {
+  local GIT_DIR="$(git rev-parse --git-dir 2> /dev/null)"
+  if [ -n $GIT_DIR ] && test -r $GIT_DIR/MERGE_HEAD; then
+    echo $GIT_PROMPT_MERGING
+  fi
 }
 
-git_prompt_info () {
- ref=$($git symbolic-ref HEAD 2>/dev/null) || return
- echo "${ref#refs/heads/}@$(git_head_id)"
-}
-
-git_dirty() {
-  # check if we're in a git repo
-  command git rev-parse --is-inside-work-tree &>/dev/null || return
-  # check if it's dirty
-  st=$($git status 2>/dev/null | tail -n 1)
-
-  if [[ $st == "" ]]
-  then
-    echo ""
+function git_untracked_files() {
+  if [[ -n $(git ls-files --other --exclude-standard 2> /dev/null) ]]; then
+    echo "$GIT_PROMPT_UNTRACKED"
   else
-    if [[ "$st" =~ ^nothing ]]
-    then
-      echo "%{$fg_bold[green]%}$(git_prompt_info)%{$reset_color%}"
-    else
-      echo "%{$fg_bold[red]%}$(git_prompt_info)%{$reset_color%}"
+    echo $GIT_PROMPT_NULL_STATE
+  fi
+}
+
+function git_modified_files() {
+  if ! git diff --quiet 2> /dev/null; then
+    echo "$GIT_PROMPT_MODIFIED"
+  else
+    echo "$GIT_PROMPT_NULL_STATE"
+  fi
+}
+
+function git_staged_files() {
+  if ! git diff --cached --quiet 2> /dev/null; then
+    echo "$GIT_PROMPT_STAGED"
+  else
+    echo "$GIT_PROMPT_NULL_STATE"
+  fi
+}
+
+function git_prompt_status() {
+  local branch="$(parse_git_branch)"
+  [ -n "$branch" ] && echo "%{$fg[yellow]%}${branch#(refs/heads/|tags/)}%{$reset_color%} [$(git_has_tracking_branch)$(git_num_commits_ahead)$(git_num_commits_behind)] [$(git_untracked_files)$(git_modified_files)$(git_staged_files)]"
+}
+
+DEFAULT_PROMPT="%{$fg_bold[blue]%}%n%{$reset_color%}@%{$fg_bold[yellow]%}%m%{$reset_color%} %{$fg[white]%}%2/%{$reset_color%} %{$fg_bold[red]%}❯%{$reset_color%}%{$fg_bold[yellow]%}❯%{$reset_color%}%{$fg_bold[green]%}❯%{$reset_color%} "
+DEFAULT_RPROMPT=""
+
+export PROMPT="$DEFAULT_PROMPT"
+export RPROMPT="$DEFAULT_RPROMPT"
+
+# The pid of the async prompt process and the communication file
+ASYNC_PROMPT=0
+ASYNC_PROMPT_FILE="/tmp/.zsh_tmp_prompt_$$"
+ASYNC_RPROMPT_FILE="/tmp/.zsh_tmp_rprompt_$$"
+
+# This here implements the async handling of the prompt.  It
+# runs the expensive git parts in a subprocess and passes the
+# information back via tempfile.
+function prompt_precmd() {
+  command_exit="$?"
+
+  function async_prompt() {
+    echo -n $DEFAULT_PROMPT > $ASYNC_PROMPT_FILE
+    echo -n $DEFAULT_RPROMPT$' '$(git_prompt_status) > $ASYNC_RPROMPT_FILE
+    if [[ x$command_exit != x0 ]]; then
+      echo -n " exited %{$fg_bold[red]%}$command_exit%{$reset_color%}" >> $ASYNC_RPROMPT_FILE
     fi
+
+    # signal parent
+    kill -s USR1 $$
+  }
+
+  # If we still have a prompt async process we kill it to make sure
+  # we do not backlog with useless prompt things.  This also makes
+  # sure that we do not have prompts interleave in the tempfile.
+  if [[ "${ASYNC_PROMPT}" != 0 ]]; then
+    kill -s HUP $ASYNC_PROMPT >/dev/null 2>&1 || :
   fi
+
+  # start background computation
+  async_prompt &!
+  ASYNC_PROMPT="$!"
 }
 
-unpushed() {
-  unpushed_count=$($git cherry -v @{upstream} | wc -l)
-  echo "${unpushed_count##*( )}"
+# This is the trap for the signal that updates our prompt and
+# redraws it.  We intentionally do not delete the tempfile here
+# so that we can reuse the last prompt for successive commands
+function prompt_trapusr1() {
+  RPROMPT="$(cat $ASYNC_RPROMPT_FILE)"
+  PROMPT="$(cat $ASYNC_PROMPT_FILE)"
+  ASYNC_PROMPT=0
+  zle && zle reset-prompt
 }
 
-need_push () {
-  unpushed_count="$(unpushed)"
-  if [[ "$unpushed_count" == "0" ]]
-  then
-    echo ""
-  else
-    echo "[ahead %{$fg_bold[green]%}${unpushed_count}%{$reset_color%}] "
-  fi
+# Make sure we clean up our tempfile on exit
+function prompt_zshexit() {
+  rm -f "$ASYNC_RPROMPT_FILE"
 }
 
-directory_name(){
-  echo "%{$fg[cyan]%}%2/%{$reset_color%}"
-}
-
-export PROMPT=$'%{$fg_bold[yellow]%}$(get_hostname)%{$reset_color%} $(directory_name) %{$fg_bold[red]%}❯%{$reset_color%}%{$fg_bold[yellow]%}❯%{$reset_color%}%{$fg_bold[green]%}❯%{$reset_color%} '
-
-precmd () {
-  export RPROMPT=$'$(need_push)$(git_dirty)'
-}
+# Hook our precmd and zshexit functions and USR1 trap
+precmd_functions+=(prompt_precmd)
+zshexit_functions+=(prompt_zshexit)
+trap prompt_trapusr1 USR1
