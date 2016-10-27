eb04ba9d0c27e574eba86eba848c23bcb2ce5505
Justin Morris <desk@pixelbloom.com>
2016-10-28 10:22:34 +1100

    Fix paths for zsh-syntax-highlighting & zsh-history-substring-search

--- zsh/zsh-history-substring-search.zsh
+++ zsh/zsh-history-substring-search.zsh
@@ -1,5 +1,5 @@
 # Assumes that zsh-history-substring-search is being provided via homebrew/boxen
-source `brew --prefix`/opt/zsh-history-substring-search/zsh-history-substring-search.zsh
+source /usr/local/opt/zsh-history-substring-search/zsh-history-substring-search.zsh
 
 # bind UP and DOWN arrow keys
 bindkey '^[[A' history-substring-search-up
--- zsh/zsh-syntax-highlighting.zsh
+++ zsh/zsh-syntax-highlighting.zsh
@@ -1,2 +1,1 @@
-# Assumes that zsh-syntax-highlighting is being provided via homebrew/boxen
-source `brew --prefix`/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
