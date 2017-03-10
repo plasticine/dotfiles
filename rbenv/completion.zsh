920ec721ef4763c359491bb2869deca5f00eb239
Justin Morris <desk@pixelbloom.com>
2017-03-10 17:43:14 +1100

    Substring search stuff.

--- rbenv/completion.zsh
+++ rbenv/completion.zsh
@@ -1,1 +1,6 @@
-[ -f /opt/boxen/rbenv/completions/rbenv.zsh ] && source /opt/boxen/rbenv/completions/rbenv.zsh
+# Assumes that zsh-history-substring-search is being provided via homebrew/boxen
+plugin="$(brew --prefix)/opt/rbenv/completions/rbenv.zsh"
+
+if [ -f $plugin ]; then
+  source $plugin
+fi
