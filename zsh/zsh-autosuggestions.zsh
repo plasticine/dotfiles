df4a255f9250256fcdd4ddcea0586f666e22e34a
Justin Morris <desk@pixelbloom.com>
2017-03-12 11:24:52 +1100

    Re-enable zsh-autosuggestions for now

--- zsh/zsh-autosuggestions.zsh
+++ zsh/zsh-autosuggestions.zsh
@@ -1,10 +1,10 @@
 # Assumes that zsh-autosuggestions is being provided via homebrew/boxen
-# plugin="$(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
+plugin="$(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
 
-# export ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=20
+export ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=20
 
-# if [ -f $plugin ]; then
-#   source $plugin
-# else
-#   echo "WARNING: zsh-autosuggestions not found, skipping."
-# fi
+if [ -f $plugin ]; then
+  source $plugin
+else
+  echo "WARNING: zsh-autosuggestions not found, skipping."
+fi
