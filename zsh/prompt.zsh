57e7889471cb00ae7a0d1d7a5af669728f485acb
Justin Morris <desk@pixelbloom.com>
2018-08-08 13:46:44 +1000

    Fixed git status for prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,7 +1,3 @@
-setopt prompt_subst
-setopt prompt_percent
-ZLE_RPROMPT_INDENT=0
-
 function welcome() {
   print -P "%F{white}
 %B%D{%A %f %B %Y, %H:%M:%S %p}%b
@@ -26,10 +22,6 @@ else
 fi
 
 GIT_PROMPT_NO_UPSTREAM="%K{red}%F{black}%B \U0000f655 UPSTREAM %b%f%k"
-# GIT_PROMPT_UNTRACKED="%F{red}\U0000f0c8%f"
-# GIT_PROMPT_MODIFIED="%F{yellow}\U0000f0c8%f"
-# GIT_PROMPT_STAGED="%F{green}\U0000f0c8%f"
-# GIT_PROMPT_NULL_STATE="%F{black}\U0000f096%f"
 
 function in_repo() {
   if $git branch 2>/dev/null; then
@@ -59,8 +51,6 @@ function git_num_commits_ahead() {
   local commits_ahead="$($git log --oneline @{u}.. 2> /dev/null | wc -l | tr -d ' ')"
   if [ "$commits_ahead" -gt 0 ]; then
     echo -ne "\U0000f55c${commits_ahead}"
-  else
-    echo -ne "\U0000f55c${commits_ahead}"
   fi
   unset commits_ahead
 }
@@ -69,47 +59,32 @@ function git_num_commits_behind() {
   local commits_behind="$(git log --oneline ..@{u} 2> /dev/null | wc -l | tr -d ' ')"
   if [ "$commits_behind" -gt 0 ]; then
     echo -ne "\U0000f544${commits_behind}"
-  else
-    echo -ne "\U0000f544${commits_behind}"
   fi
   unset commits_behind
 }
 
-# function git_untracked_files() {
-#   if [[ -n $(git ls-files --other --exclude-standard 2> /dev/null) ]]; then
-#     echo -ne "$GIT_PROMPT_UNTRACKED"
-#   else
-#     echo -ne "$GIT_PROMPT_NULL_STATE"
-#   fi
-# }
+function diffstat_to_master() {
+  local diff="$($git diff --cached --shortstat origin/master)"
+  local changed="$(echo "$diff" | awk '{print $1}')"
+  local inserted="$(echo "$diff" | awk '{print $4}')"
+  local deleted="$(echo "$diff" | awk '{print $6}')"
 
-# function git_modified_files() {
-#   if ! git diff --quiet 2> /dev/null; then
-#     echo -ne "$GIT_PROMPT_MODIFIED"
-#   else
-#     echo -ne "$GIT_PROMPT_NULL_STATE"
-#   fi
-# }
+  echo -ne "%K{blue} ${changed} %k%K{green} ${inserted} %k%K{red} ${deleted} %k"
+  unset diff changed inserted deleted
+}
 
-# function git_staged_files() {
-#   if ! git diff --cached --quiet 2> /dev/null; then
-#     echo -ne "$GIT_PROMPT_STAGED"
-#   else
-#     echo -ne "$GIT_PROMPT_NULL_STATE"
-#   fi
-# }
 
 function git_status() {
   local ref_stats="$(git_branch_raw)@$(git_commit)"
-  local remote_stats="$(git_num_commits_ahead) $(git_num_commits_behind)"
-  # local file_stats="$(git_untracked_files) $(git_modified_files) $(git_staged_files)"
-  local file_stats="?0 !23 @12"
+  local remote_stats="$(git_num_commits_ahead)$(git_num_commits_behind)"
+  local file_stats="$(diffstat_to_master)"
   local remote_tracking="$(git_has_tracking_branch)"
 
-  echo -ne "%K{white}%F{black} \U0000e727 ${ref_stats}・${file_stats}・${remote_stats} ${remote_tracking}%f %k"
+  echo -ne "%K{white}%F{black} \U0000e727 ${ref_stats} ${remote_stats} ${file_stats}%f%k"
+  unset ref_stats remote_stats file_stats remote_tracking
 }
 
-DEFAULT_PROMPT="%F{white}%D{%H:%M:%S} %F{magenta}%n%f%F{white}@%f%B%F{yellow}%M%f%b %F{white}%3~%f $(echo -ne '\U0000f423') "
+DEFAULT_PROMPT="%F{white}%D{%H:%M:%S} %F{magenta}%n%f%F{white}@%f%B%F{yellow}%M%f%b %F{white}%3~%f $(echo -ne '\U0000f054') "
 DEFAULT_RPROMPT=""
 NEXT_PROMPT=""
 NEXT_RPROMPT=""
